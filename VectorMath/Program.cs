a58f8cce5800923ee27141f94732871cea24466b
Agustin Rodriguez <agustinr@ksu.edu>
2019-12-09 16:17:59 -0600

    This stuff is super cool

--- VectorMath/Program.cs
+++ VectorMath/Program.cs
@@ -7,6 +7,37 @@ namespace VectorMath
         static void Main(string[] args)
         {
             Console.WriteLine("Fun with vectors");
+
+            Vector3 one = new Vector3(3, 4, 5);
+            Vector3 two = new Vector3(4, 8, 0);
+
+            Console.WriteLine($"Static method Add: {Vector3.Add(one, two)}");
+
+            Console.WriteLine($"Object method Add: {one.Add(two)}");
+
+            Console.WriteLine($"Operator: {one + two}");
+
+            Console.WriteLine($"Operator Subraction: {one - two}");
+
+            Console.WriteLine($"Scalar multiplication: {2 * one}");
+
+            Console.WriteLine($"Vector Comparison: {one == two}");
+
+            Console.WriteLine($"Vector Comparison: {one == new Vector3(3, 4, 5)}");
+
+            Console.WriteLine($"Preincrement: {++one}");
+            Console.WriteLine($"Preincrement: {one++}");
+
+            Console.WriteLine($"Magnitude: {~one}");
+
+            if (Vector3.Zero)
+            {
+                Console.WriteLine($"{one} is Truthy");
+            }
+            else
+            {
+                Console.WriteLine($"{one} is Falsy");
+            }
         }
     }
 }
